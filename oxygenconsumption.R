7a4dd0b6dd3cf150b57a632807d3cbae95543dc3
Alanna Bachtlin <71525636+bachtlina@users.noreply.github.com>
2022-01-14 12:17:34 -0500

    Create oxygenconsumption.R

---
+++ oxygenconsumption.R
@@ -0,0 +1,25 @@
+#To analyze data for oxygen consumption equation
+
+library(dplyr)
+library(ggplot2)
+
+setwd("/Users/alannabachtlin/Desktop/data/")
+DO_BB629 <- data.frame(read.csv("DOBBlogprofile.csv"))
+
+vel.dif <- array(NA, dim = c((nrow(DO_BB629)-1),(nrow(DO_BB629))))
+do.dif <- vel.dif
+height <- vel.dif
+#u2-u1
+for(j in 2:(nrow(DO_BB629)-1))  {
+  for(i in (j+1):nrow(DO_BB629)) {
+    vel.dif[j,i] <- DO_BB629$U.m.s.[i] - DO_BB629$U.m.s.[j]
+    do.dif[j,i] <- DO_BB629$Avg.DO..mg.l.[i] - DO_BB629$Avg.DO..mg.l.[j]
+    height[j,i] <- log(DO_BB629$z..m.[i] / DO_BB629$z..m.[j])
+  }
+}
+
+plot(do.dif, height)
+plot(vel.dif, height)
+
+ggplot(DO_BB629) +
+  geom_point(aes(x=Avg.DO..mg.l.,y=z..m.))
