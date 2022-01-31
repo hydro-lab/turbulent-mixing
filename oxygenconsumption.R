ee6552098a3df5dec342d81ca213d7bfd80119ad
Alanna Bachtlin <71525636+bachtlina@users.noreply.github.com>
2022-01-30 21:16:36 -0500

    Update oxygenconsumption.R

--- oxygenconsumption.R
+++ oxygenconsumption.R
@@ -10,7 +10,7 @@ vel.dif <- array(NA, dim = c((nrow(DO_BB629)-1),(nrow(DO_BB629))))
 do.dif <- vel.dif
 height <- vel.dif
 #u2-u1
-for(j in 2:(nrow(DO_BB629)-1))  {
+for(j in 1:(nrow(DO_BB629)-1))  {
   for(i in (j+1):nrow(DO_BB629)) {
     vel.dif[j,i] <- DO_BB629$U.m.s.[i] - DO_BB629$U.m.s.[j]
     do.dif[j,i] <- DO_BB629$Avg.DO..mg.l.[i] - DO_BB629$Avg.DO..mg.l.[j]
